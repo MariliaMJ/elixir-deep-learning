465fb31f56b2d01c1540e1da60e54225c5551140
Marília M. Janizelli <mjanizelli@xerpa.com.br>
2018-07-23 11:30:48 -0300

    Project start/dependencies

---
+++ deepnet/mix.exs
@@ -0,0 +1,36 @@
+defmodule Deepnet.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :deepnet,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger,
+      :sfmt,
+      :numerix,
+      :matrix
+      ],
+      mod: {Deepnet.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:matrix,"~> 0.3.2"},
+      {:numerix, "~> 0.4.2"},
+      {:sfmt}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+end
